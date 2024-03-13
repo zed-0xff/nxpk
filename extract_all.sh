1434d77244f0ee6554695726e98f0127b607900d
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2024-03-13 08:13:34 +0200

    initial

---
+++ extract_all.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash -e
+# ^^^^^^^^^^^^ automatically use bash 5.x from homebrew on OSX
+
+GAME_DIR=1.115
+
+shopt -s globstar
+cd $GAME_DIR
+
+echo "[.] removing 'out' dir .."
+rm -rf out
+
+../nxpk.rb x **/*.npk
+
+echo "[.] derotoring nxs scripts .."
+docker run -ti -v .:/data derotor
+
+echo "[.] decrypting redirect.nxs and neox.xml .."
+../decrypt_xml.rb out/script.npk/redirect.nxs neox.xml
+
+echo "[.] dexoring nxs scripts .."
+../dexor_pyc.rb out/script.npk/redirect.nxs.decrypted out/script.npk/**/*.pyc
+
+echo "[=] Done"
